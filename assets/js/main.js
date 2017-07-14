f867f6755a70270654b4ba74493c48a46366815e
Diana Villasmil <diacavipi@gmail.com>
2017-07-14 00:20:41 -0400

    index listo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
 /* Holiii acá va tu código también */
+
+function login(){
+    var mail = $('#mail');
+    var pass = $('#pass');
+    if(mail.hasClass('valid') && pass.hasClass('valid')) {
+        localStorage.setItem("mail", mail.val());
+        window.location.href = "home.html";
+    }
+}
